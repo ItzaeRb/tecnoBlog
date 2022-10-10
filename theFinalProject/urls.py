e27764a287b1c297d88dab1054f3c06b0843289a
Itzae Rivera <itzae@ives.edu.mx>
2022-10-10 00:21:35 -0500

    Estructura inicial del homepage

--- theFinalProject/urls.py
+++ theFinalProject/urls.py
@@ -26,6 +26,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage),
-     path('tecnoBlog/', include("tecnoBlog.urls"))
+    path('tecnoBlog/', include("tecnoBlog.urls"))
 
 ]
--- theFinalProject/view.py
+++ theFinalProject/view.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def homePage(self, name):
-    return HttpResponse(f'HomePage')
+def homePage(request):
+    return HttpResponse(f'Home')
