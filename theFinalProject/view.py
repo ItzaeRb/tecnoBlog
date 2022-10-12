417c812138b0501722cb9adf532e46f6fe5a5b1c
Itzae Rivera <itzae@ives.edu.mx>
2022-10-11 23:29:39 -0500

    Estructura html para las templates

--- theFinalProject/view.py
+++ theFinalProject/view.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 def homePage(request):
-    return HttpResponse(f'Home')
+    return render(request, "homePage.html")
