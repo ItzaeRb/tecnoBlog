5e87a598967d3b473eca4eb30ff38ad583a21c97
Itzae Rivera <itzae@ives.edu.mx>
2022-10-11 23:49:43 -0500

    Creación de los modelos

--- tecnoBlog/models.py
+++ tecnoBlog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    idUsuario = models.IntegerField()
+    nombre = models.CharField(max_length=40)
+    apellidoPaterno = models.CharField(max_length=60)
+    apellidoMaterno = models.CharField(max_length=60)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=10)
+    tipoUsuario = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=100)
+
+class Blogs(models.Model):
+    idBlog = models.IntegerField()
+    titulo = models.CharField(max_length=40)
+    subtitulo = models.CharField(max_length=60)
+    contenido = models.CharField(max_length=500)
+    autor = models.CharField(max_length=60)
+    fecha = models.DateField()
+
+class Mensajes(models.Model):
+    idMensaje = models.IntegerField()
+    destinatario = models.CharField(max_length=60)
+    contenido = models.CharField(max_length=140)
+    remitente = models.CharField(max_length=60)
+    fecha = models.DateField()
