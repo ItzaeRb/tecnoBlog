a5a48e33d1c7c315f4c52e71f1eaa291dfe82c1e
Jeremias Acurso <jeremias.acurso@hotmail.com>
2022-10-12 18:23:56 -0300

    importamos todas las views de la aplicacion y c reamos ruta al login y inicio

--- tecnoBlog/urls.py
+++ tecnoBlog/urls.py
@@ -1,8 +1,9 @@
 from re import template
 from django.urls import path
-from tecnoBlog.views import *
+from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', inicio),
+    path('login/', login_request)
 ]
