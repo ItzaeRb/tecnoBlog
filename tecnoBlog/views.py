ff2eca2b148554c44dfd45a81a244118a075e7f7
Jeremias Acurso <jeremias.acurso@hotmail.com>
2022-10-12 18:25:04 -0300

    creamos views para funcion de login y de registro

--- tecnoBlog/views.py
+++ tecnoBlog/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from curses.ascii import HT
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,  AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 def inicio(request):
     return render(request, "homePage.html")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            user = form.cleaned_data.get("username")
+            pwd = form.cleaned_data.get("password")
+            user = AuthenticationForm(username = user, password = pwd)
+            if user is not None:
+                login_request(request, user)
+                return render(request, "homePage.html")
+            else:
+                return render(request, "login.html", {"form": form})
+        return render(request, "login.html", {"form": form})
+    form = AuthenticationForm()
+    return render(request, "login.html", {"form":form})
+
+def registroUsuario(request):
+    form = UserRegisterForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            #username = form.cleaned_data["username"]
+            form.save()
+            return redirect("/login")
+        else:
+            return render(request, "registroUsuario.html", {"form":form})
+    form = UserRegisterForm()
+    return render(request, "registroUsuario.html", {"form":form})
+
+
+
+#def login(request):
+#    if request.method == 'GET':
+#        return render(request, 'login.html', {'form': UserCreationForm})
+#    else:
+#        if request.POST['password1'] ==  request.POST['password2']:
+#            try:        
+#                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+#                user.save() # lo guarda dentro de la base de datos
+#                return HttpResponse('Usuario creado')
+#            except:
+#                return HttpResponse('no  pudo crear el usuario')
+#        return HttpResponse('Pass do not match')
