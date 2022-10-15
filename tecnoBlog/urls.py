d54141a4006f64cd3efd868beb844972f17d10cc
Itzae Rivera <itzae@ives.edu.mx>
2022-10-14 19:48:08 -0500

    Se crea el formulario de registro de usuarios

--- tecnoBlog/urls.py
+++ tecnoBlog/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', inicio),
     path('login/', login_request),
+    path('signIn/', registroUsuario),
     path('tecnoBlog/nuevoPost', nuevoPost),
     path('tecnoBlog/verPosts', verPosts),
 ]
