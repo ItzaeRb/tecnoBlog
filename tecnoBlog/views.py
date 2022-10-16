1053d4f673f2f99462dac21473c2d43691531db5
Jeremias Acurso <jeremias.acurso@hotmail.com>
2022-10-15 22:17:46 -0300

    agregamos funciones de signin signup y logout

--- tecnoBlog/views.py
+++ tecnoBlog/views.py
@@ -2,52 +2,41 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,  AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from tecnoBlog.forms import UserRegisterForm
-
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def inicio(request):
     return render(request, "homePage.html")
 
-def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data = request.POST)
-        if form.is_valid():
-            user = form.cleaned_data.get("username")
-            pwd = form.cleaned_data.get("password")
-            user = AuthenticationForm(username = user, password = pwd)
-            if user is not None:
-                login_request(request, user)
-                return render(request, "homePage.html")
-            else:
-                return render(request, "login.html", {"form": form})
-        return render(request, "login.html", {"form": form})
-    form = AuthenticationForm()
-    return render(request, "login.html", {"form":form})
+def signup(request):
+    if request.method == 'GET':
+            return render(request, 'signup.html', {'form':UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #de la clase User voy a utilizar de todos sus objetos un metodo llamado create_user espera el usuario y contrase;a que vienen de request.POST['username']
+                #print(request.POST)
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save() # creamos el user, y lo guardamos en base de datos con el save()
+                login(request, user) #autentica el usuario, login es un metodo viene de django.
+                return redirect(signin) #lo envia a la funcion inicio
+            except: #si no se puede crear usuario, renderiza el signup y muestra el error
+                return render(request, 'signup.html', {'form':UserCreationForm, 'error':'No se pudo crear el usuario, intente nuevamente'})
+        return render(request, 'signup.html', {'form':UserCreationForm, 'error':'Las pass no coinciden'})
 
-def registroUsuario(request):
-    form = UserRegisterForm(request.POST)
-    if request.method == "POST":
-        if form.is_valid():
-            #username = form.cleaned_data["username"]
-            form.save()
-            return redirect("/login")
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form':AuthenticationForm})
+    else:
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is None: #si la autenticacion no fue valida.
+            return render(request, 'signin.html', {'form':AuthenticationForm, 'error': 'No se pudo validar el usuario'})
         else:
-            return render(request, "registroUsuario.html", {"form":form})
-    form = UserRegisterForm()
-    return render(request, "registroUsuario.html", {"form":form})
+            login(request, user)#loguea al usuario ya atutenicado y guarda la sesion
+            return redirect('/homePage/')
 
 
-
-#def login(request):
-#    if request.method == 'GET':
-#        return render(request, 'login.html', {'form': UserCreationForm})
-#    else:
-#        if request.POST['password1'] ==  request.POST['password2']:
-#            try:        
-#                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
-#                user.save() # lo guarda dentro de la base de datos
-#                return HttpResponse('Usuario creado')
-#            except:
-#                return HttpResponse('no  pudo crear el usuario')
-#        return HttpResponse('Pass do not match')
+def signout(request):
+    logout(request)
+    return redirect(inicio)
