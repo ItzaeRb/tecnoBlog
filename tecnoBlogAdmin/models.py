bc7d1907bcd90392e4d691c31083d1f446417818
Itzae Rivera <itzae@ives.edu.mx>
2022-10-16 23:43:45 -0500

    migracion de un nuevo modelo en la app admin

--- tecnoBlogAdmin/models.py
+++ tecnoBlogAdmin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    idBlog = models.IntegerField()
+    titulo = models.CharField(max_length=40)
+    subtitulo = models.CharField(max_length=60)
+    contenido = models.CharField(max_length=500)
+    autor = models.CharField(max_length=60)
+    fecha = models.DateField()
+    imagen = models.ImageField()
+
+    def __str__(self):
+        return f"ID:{self.idBlog} - Título:{self.titulo} - Subtítulo:{self.subtitulo} - Contenido: {self.contenido} - Autor: {self.autor} - Fecha: {self.fecha} - Imagen: {self.imagen}"
+
